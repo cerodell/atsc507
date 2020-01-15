67b1e237d4c973fc30cf7552a12b5987209e8f00
Chris Rodell <crodell@eoas.ubc.ca>
2020-01-15 15:02:09 -0800

    Pressure solved

--- py/hw1_hybrid_eta.py
+++ py/hw1_hybrid_eta.py
@@ -6,8 +6,9 @@ import matplotlib.colors as colors
 from cr507.utils import plt_set
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
+# %%
 
-## COnstants
+## Constants
 a = 0.0293 ## 0.0293 km/K
 
 
@@ -26,6 +27,7 @@ z_si = 1 + np.cos(2.*(3.14159)*(x-500.) / 500.)
 ## apply condtion for mnt location
 z_surf = np.where((250 < x) & (x < 750), z_si, 0 )
 
+# %%
 
 ## Solve for temperature across domain 
 T_trop = (40 - 0.08 * xx) - 6.5*zz  ## Within hypotehtical troposphere
@@ -35,27 +37,47 @@ T_iso = (40 - 0.08 * xx) - 6.5*12   ## Above hypotehtical troposphere (isotherma
 T = np.where(zz < 12, T_trop, T_iso)
 ## Convert to Kelvin for later calcualtions
 T = T + 273.15
+T = np.vstack((T, T[-1,:]))
 
+# T_avg = []
+# for i in range(len(z)):
+#     T_avg_i = (T[i,:]+T[i+1,:])/2.
+#     T_avg.append(T_avg_i)
+# T_avg = np.stack(T_avg)
+
+# %%
 ## Solve for pressure across the surface (x) of the domain (ie z = 0)
 P_slp = 95 + 0.01 * x
 
-## Solve for presure at hieght applying the hypsometric equation
-P = []
-T_avg = np.cumsum(T, axis=0)
-# P_2 = P_1 * np.exp((dz)/a*T_avg)
+# def pressure(P_1,T):
 
-T_test = [sum(T[i::1,0]) for i in range(len(T))]
-
-
-# def avg(x):
-#     x = [i for i in x if i is not None]
-#     return sum(x, 0.0) / len(x)
-
-# filt = imap(avg, izip_longest(*T))
+#     if P_1[0] == P_slp[0]:
+#         return P_slp
+#     else:
+#         P_2 = P_1 * np.exp((-dz)/(a*T))
+#         return P_2
 
 
 
+def pressure(P_1,T):
+    P_2 = P_1 * np.exp((-dz)/(a*T))
+    return P_2
 
+P_list, T_avg = [], []
+for i in range(len(z)):
+    P_list.append(P_slp)
+    T_avg_i = (T[i,:]+T[i+1,:])/2.
+    T_avg.append(T_avg_i)
+    P_2 = pressure(P_slp,T_avg_i)
+    P_slp = P_2
+
+
+
+
+T_avg = np.stack(T_avg)
+P_final = np.stack(P_list)
+
+# %%
 
 
 
@@ -77,6 +99,7 @@ divider = make_axes_locatable(ax)
 cax = divider.append_axes("right", size="5%", pad=0.05)
 C = ax.contourf(xx, zz, T, cmap = 'coolwarm', norm = Cnorm, levels = v)
 clb = plt.colorbar(C,cax=cax)
+
 # clb.set_label('Relative Humidity Gradient \n(%)', fontsize =ylabel)
 # clb.ax.tick_params(labelsize=tick_size) 
 # clb.set_alpha(.95)
