9469df9fc166e3900f031c9e15f8c1b3f33b9530
cerodell <rodell@colorado.edu>
2020-01-21 09:41:21 -0800

    added markdown cells

--- py/hw1_hybrid_eta.py
+++ py/hw1_hybrid_eta.py
@@ -1,5 +1,7 @@
 # %% [markdown]
 
+# ## ATSC 507 Homework 1   -   Hybrid-Eta Exercise 
+# ### Chris Rodell
 
 # %%
 import context
@@ -9,6 +11,8 @@ import matplotlib.pyplot as plt
 import matplotlib.colors as colors 
 from cr507.utils import plt_set
 from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+%matplotlib
 
 # %%
 
@@ -33,7 +37,7 @@ z_ground = np.where((250 < x) & (x < 750), z_si, 0 )
 
 # %% [markdown]
 
-
+# ### Set up your domain
 
 # %%
 
@@ -51,6 +55,7 @@ T = np.vstack((T, T[-1,:]))
 
 # %% [markdown]
 
+# ### Solve for pressure across the surface (x) of the domain (ie z = 0)
 
 
 # %%
@@ -77,16 +82,18 @@ T_avg = np.stack(T_avg)
 P_final = np.stack(P_list)
 
 # %% [markdown]
-
+# ### Question 1
+# Find:  On an x-z graph, plot the altitudes (km) of the following isobaric surfaces:
+# 100, 90, 80, 70, 60, 50, 40, 30, 20, 10, 5, 2 kPa.
+# On the same plot, plot the altitude of Zground. 
 
 # %%
-# %matplotlib
 
 
 
 ## Set up Figure for plotting multiple variable on one graph
 fig, ax = plt.subplots(1,1, figsize=(10,5))
-fig.suptitle('Eta something...', fontsize= plt_set.title_size, fontweight="bold")
+fig.suptitle('Pressure (kPa) at height (km)', fontsize= plt_set.title_size, fontweight="bold")
 divider = make_axes_locatable(ax)
 cax = divider.append_axes("right", size="5%", pad=0.05)
 
@@ -100,7 +107,7 @@ def confil(var):
 
 ## Plot mnt
 ax.fill_between(x,0, z_ground, color = 'saddlebrown', zorder = 4)
-
+ax.set(xlabel='Horizonatal distance (km)',ylabel='Vertical height (km)')
 ## Plot isobars
 v_line = [2,5,10,20,30,40,50,60,70,80,90,100]
 CS = ax.contour(xx, zz, P_final, levels = v_line, colors = 'black', zorder = 10)
@@ -115,53 +122,72 @@ clb.ax.tick_params(labelsize= plt_set.tick_size)
 clb.set_alpha(.95)
 clb.draw_all()
 
-
-
-# divider = make_axes_locatable(ax)
-# cax = divider.append_axes("right", size="5%", pad=0.05)
-# C = ax.contourf(xx, zz, T, cmap = 'coolwarm', norm = Cnorm, levels = v)
-# clb = plt.colorbar(C,cax=cax, extend='both')
-# clb.set_label('Pressure kPa', fontsize = plt_set.label)
-# clb.ax.tick_params(labelsize= plt_set.tick_size) 
-# clb.set_alpha(.95)
-# clb.draw_all()
-
-#ax.set_xlabel("Wind Speed $(ms^-1)$", fontsize= plt_set.label)
-#ax.set_ylabel("Height Above Ground Level  \n (AGL)", fontsize= plt_set.label)
-#ax.tick_params(axis='both', which='major', labelsize= plt_set.tick_size)
-#ax.xaxis.grid(color='gray', linestyle='dashed')
-#ax.yaxis.grid(color='gray', linestyle='dashed')
-#ax.set_facecolor('lightgrey')
-#ax.legend(loc='best')
    
 
 
 # %% [markdown]
+# ### Question 2)
+# 2) Interpolate to find the Psurface (kPa) pressure at Zground.
+# Namely, it is the pressure that corresponds to eta = 1.  
+# This pressure that you use to find eta in exercises (3) & (4).
 
-# Solve for Surface Pressure at gorund level
-
-
+# Present the results in a table, where:  
+# row1 = x, (km)
+# row2 = Zground, (km)
+# row3=Psfc (kPa)
 # %%
 
-
-
+## Solve for pressure 
 P_surf = P_final[0,:] * np.exp((zz[0,:]-z_ground)/(a*T_avg[0,:]))
 
+## Set up table (sorry they are in columns not rows)
 values = {'x(km)': x, 'Zground(km)': z_ground, 'Psfc(kPa)':P_surf}
 
 df = pd.DataFrame(values, columns = ['x(km)', 'Zground(km)', 'Psfc(kPa)'])
 
 save = str(context.data_dir)  + '/Q2_Table.csv'
 
+## Save table as csv file
 df.to_csv(save)
-# str(context.data_dir)
-# df
+
+## Display table in notebook
+df
 
 # %% [markdown]
+# ### Question 3)
+# Create a new P-x graph, on which you plot lines of constant eta,
+# for the eta values listed below.  Namely, it should look something 
+# like WRF4 figure 2.1b, but with the more realistic meteorology that 
+# I prescribed above.  Also, like that figure, plot pressure P on the
+# vertical axis in reversed order (highest pressure at the bottom of
+# the figure), but don't use a log scale for P.
+# <br />
+# <br />
+# CAUTION: when calculating the values of B to use in WRF4 eq. (2.2),
+# be advised that WRF4 eq. (2.3) applies only for eta > eta_c. Otherwise, set B = 0 for eta <= eta_c.
+# <br />
 
-# Solve for Eta using the equations from WRFv4 Tech Notes
+# <br>
+# eta = 0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.85, 0.9, 0.95, 1
+# <br />
 
+# <br />
+# <br />
 
+# - (2.2)
+# $$
+# p_{d}=B(\eta)\left(p_{s}-p_{t}\right)+[\eta-B(\eta)]\left(p_{0}-p_{t}\right)+p_{t} (2.2)
+# $$
+# 
+# - (2.3)
+# $$
+# B(\eta)=c_{1}+c_{2} \eta+c_{3} \eta^{2}+c_{4} \eta^{3} 
+# $$
+# 
+# - (2.5)
+# $$
+# c_{1}=\frac{2 \eta_{c}^{2}}{\left(1-\eta_{c}\right)^{3}}, \quad c_{2}=\frac{-\eta_{c}\left(4+\eta_{c}+\eta_{c}^{2}\right)}{\left(1-\eta_{c}\right)^{3}}, \quad c_{3}=\frac{2\left(1+\eta_{c}+\eta_{c}^{2}\right)}{\left(1-\eta_{c}\right)^{3}}, \quad c_{4}=\frac{-\left(1+\eta_{c}\right)}{\left(1-\eta_{c}\right)^{3}}
+# $$
 
 
 
@@ -200,13 +226,11 @@ P_d_final = P_d_final[::-1,:]
 
 # %%
 
-%matplotlib
 plt.plot(eta)
 
 
 # %%
 
-# %matplotlib
 
 
 
