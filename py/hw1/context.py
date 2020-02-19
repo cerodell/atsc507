9302bbdcf4546d564ba7df043b51792a11996404
cerodell <rodell@colorado.edu>
2020-02-19 10:32:17 -0800

    fixed context.py to work with subfolders

--- py/hw1/context.py
+++ py/hw1/context.py
@@ -31,12 +31,15 @@ from pathlib import Path
 path = Path(__file__).resolve()  # this file
 this_dir = path.parent  # this folder
 notebooks_dir = this_dir
-root_dir = notebooks_dir.parent
+root_dir = notebooks_dir.parents[1]
 data_dir = root_dir / Path("data")
 test_dir = root_dir / Path("test_data")
 map_dir = root_dir / Path("map_data")
 
-# %%
+
+
+
+ # %%
 sys.path.insert(0, str(root_dir))
 sep = "*" * 30
 print(f"{sep}\ncontext imported. Front of path:\n{sys.path[0]}\n{sys.path[1]}\n{sep}\n")
--- py/hw2/context.py
+++ py/hw2/context.py
@@ -31,7 +31,7 @@ from pathlib import Path
 path = Path(__file__).resolve()  # this file
 this_dir = path.parent  # this folder
 notebooks_dir = this_dir
-root_dir = notebooks_dir.parent
+root_dir = notebooks_dir.parents[1]
 data_dir = root_dir / Path("data")
 test_dir = root_dir / Path("test_data")
 map_dir = root_dir / Path("map_data")
