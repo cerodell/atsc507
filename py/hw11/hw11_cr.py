14995411b3cffb55f0cbd4285cc43938ca2d99ba
Chris Rodell <crodell@eoas.ubc.ca>
2020-04-25 21:31:10 -0700

    hw 11 up to A22

---
+++ py/hw11/hw11_cr.py
@@ -0,0 +1,472 @@
+# %% [markdown]
+# # Homework 11 Verification
+# ### ATSC 507 
+# ##### Christopher Rodell 
+
+# %%
+import context
+import numpy as np 
+import matplotlib.pyplot as plt
+from cr507.utils import plt_set
+# %% [markdown]
+# A19. Given the following fields of 50-kPa height <br />
+# (km). Find the: <br />
+# <br />
+# Each field (i.e., each weather map) below covers an
+# area from North to South and West to East.
+# %%
+d   =  {"A" :np.array([[5.2, 5.3, 5.4, 5.3], [5.3, 5.4, 5.5, 5.4], [5.4, 5.5, 5.6, 5.5], [5.5, 5.6, 5.7, 5.6], [5.6, 5.7, 5.8, 5.7]]),
+        "F": np.array([[5.3, 5.4, 5.5, 5.4],[5.5, 5.4, 5.5, 5.6],[5.6, 5.6, 5.6, 5.6],[5.8, 5.7, 5.6, 5.7],[5.9, 5.8, 5.7, 5.8]]),
+        "V": np.array([[5.3, 5.3, 5.3, 5.4],[5.4, 5.3, 5.4, 5.5],[5.5, 5.4, 5.5, 5.5],[5.7, 5.5, 5.6, 5.6],[5.8, 5.7, 5.6, 5.6]]),
+        "C": np.array([[5.4, 5.4, 5.4, 5.4],[5.4, 5.4, 5.4, 5.4],[5.5, 5.5, 5.5, 5.5],[5.6, 5.6, 5.6, 5.6],[5.7, 5.7, 5.7, 5.7]])}
+
+# %% [markdown]
+# a. mean forecast error <br />
+# $$
+# M E=\overline{(F-V)}=\bar{F}-\bar{V}
+# $$
+# %%
+
+ME = np.mean(d['F']- d['V'])
+print("Mean Forecast Error:  ", round(ME,4))
+
+# %% [markdown]
+# b. mean persistence error <br />
+# $$
+# \overline{(A-V)}=\bar{A}-\bar{V}=\text { mean persistence error }
+# $$
+# %%
+
+MPE = np.mean(d['A']- d['V'])
+print("Mean Persistence Error:  ", round(MPE,4))
+
+
+# %% [markdown]
+
+# c. mean absolute forecast error <br />
+# $$
+# M A E=\overline{|F-V|}
+# $$
+# %%
+
+MAE = np.mean(abs(d['F']- d['V']))
+print("Mean Absolute Forecast Error:  ", round(MAE,4))
+
+
+# %% [markdown]
+# d. mean squared forecast error <br />
+# $$
+# M S E=\overline{(F-V)^{2}}
+# $$
+# %% 
+
+MSE = np.mean((d['F']- d['V'])**2)
+print("Mean Squared Forecast Error:  ", round(MSE,4))
+
+
+# %% [markdown]
+# e. mean squared climatology error <br />
+# $$
+# M S E C=\overline{(C-V)^{2}}
+# $$
+# %% 
+
+MSEC = np.mean((d['C']- d['V'])**2)
+print("Mean Squared Climatology Error:  ", round(MSEC,4))
+
+# %% [markdown]
+# f. mean squared forecast error skill score <br />
+# $$
+# \operatorname{MSESS}=1-\frac{\operatorname{MSE}}{\operatorname{MSEC}}
+# $$
+# %%
+
+MSESS = 1 - (MSE/ MSEC)
+print("Mean Squared Forecast Error Skill Score:  ", round(MSESS,4))
+
+# %% [markdown]
+# g. RMS forecast error <br />
+# $$
+# R M S E=\sqrt{\overline{(F-V)^{2}}}
+# $$
+# %% 
+
+RMS = np.mean((d['F']- d['V'])**2)**(0.5)
+print("RMS Forecast Error:  ", round(RMS,4))
+
+
+# %% [markdown]
+# h. correlation coefficient between forecast and verification <br />
+# $$
+# r=\frac{\overline{F^{\prime} V^{\prime}}}{\sqrt{\overline{(F^{\prime})^{2}}} \cdot \sqrt{\overline{(V^{\prime})^{2}}}}
+# $$
+# $$
+# F^{\prime}=F-\bar{F} \quad \text { and } \quad V^{\prime}=V-\bar{V}
+# $$
+# %% 
+FP = d['F'] - np.mean(d['F'])
+d.update({'FP':FP})
+VP = d['V'] - np.mean(d['V'])
+d.update({'VP':VP})
+
+r = np.mean(d['FP'] * d['VP']) / \
+    (((np.mean(d['FP']**(2)))**(0.5)) * ((np.mean(d['VP']**(2)))**(0.5)))
+print("Correlation Coefficient between Forecast and Verification:  ", round(r,4))
+
+# %% [markdown]
+# i. forecast anomaly correlation <br />
+# $$
+# \text{forecast anomaly correlation} = \frac{\overline{[(F-C)-\overline{(F-C)}] \cdot[(V-C)-\overline{(V-C)}]}}{\sqrt{[\overline{(F-C)-\overline{(F-C)}]^{2}} \cdot \overline{[(V-C)-\overline{(V-C)}]^{2}}}}
+# $$
+# %% 
+
+k1 = np.mean(((d['F']-d['C']) - np.mean(d['F']-d['C'])) * \
+        ((d['V']-d['C']) - np.mean(d['V']-d['C']))) 
+
+k2 = (np.mean((((d['F']-d['C']) - np.mean(d['F']-d['C']))**2)) * \
+        np.mean(((d['V']-d['C']) - np.mean(d['V']-d['C']))**2))**(0.5)
+FAC = k1/k2
+
+print("Forecast Anomaly Correlation:  ", round(FAC,4))
+
+# %% [markdown]
+# j. persistence anomaly correlation <br />
+# $$
+# \text{persistence anomaly correlation} = \frac{\overline{[(A-C)-\overline{(A-C)}] \cdot[(V-C)-\overline{(V-C)}]}}{\sqrt{[\overline{(A-C)-\overline{(A-C)}]^{2}} \cdot \overline{[(V-C)-\overline{(V-C)}]^{2}}}}
+# $$
+# %% 
+
+k1 = np.mean(((d['A']-d['C']) - np.mean(d['A']-d['C'])) * \
+        ((d['V']-d['C']) - np.mean(d['V']-d['C']))) 
+        
+k2 = (np.mean((((d['A']-d['C']) - np.mean(d['A']-d['C']))**2)) * \
+        np.mean(((d['V']-d['C']) - np.mean(d['V']-d['C']))**2))**(0.5)
+
+PAC = k1/k2
+
+print("Persistence Anomaly Correlation:  ", round(PAC,4))
+
+
+
+# %% [markdown]
+# k. Draw height contours by hand for each field, to show locations of ridges and troughs. <br />
+# <br />
+
+
+# %%
+
+fig, ax = plt.subplots(figsize=(7,7))
+fig.suptitle('Analysis', fontsize= plt_set.title_size, fontweight="bold")
+level = np.mean(d['A'])
+ax.contour(np.flip(d['A'],0), levels = [level])
+
+fig, ax = plt.subplots(figsize=(7,7))
+fig.suptitle('Forecast', fontsize= plt_set.title_size, fontweight="bold")
+level = np.mean(d['F'])
+ax.contour(np.flip(d['F'],0), levels = [level])
+
+fig, ax = plt.subplots(figsize=(7,7))
+fig.suptitle('Verification', fontsize= plt_set.title_size, fontweight="bold")
+level = np.mean(d['V'])
+ax.contour(np.flip(d['V'],0), levels = [level])
+
+fig, ax = plt.subplots(figsize=(7,7))
+fig.suptitle('Climate', fontsize= plt_set.title_size, fontweight="bold")
+level = np.mean(d['C'])
+ax.contour(np.flip(d['C'],0), levels = [level])
+
+# plt.show()
+
+
+# %% [markdown]
+
+# A20. Given the following contingency table, calculate all the binary verification statistics. <br />
+#  <br />
+# &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Observation <br />
+# &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Yes No <br />
+
+# Forecast Yes: 150 65 <br />
+# &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; No: 50 100<br />
+# %%
+a, b, c ,d = 150, 65, 50, 100
+# Find: B, PC, HSS, H, F, FAR, TSS, CSI, GSS
+# %% [markdown]
+# $$
+# n=a+b+c+d
+# $$
+# %%
+n = a + b + c + d
+print(n)
+
+# %% [markdown]
+# $$
+# B=\frac{a+b}{a+c}
+# $$
+
+# %%
+B = (a + b) / (a + c)
+print("bias score", B)
+
+# %% [markdown]
+# $$
+# P C=\frac{a+d}{n}
+# $$
+# %%
+
+PC = (a + d) / n
+print ("portion correct", PC)
+
+# %% [markdown]
+# $$
+# E=\left(\frac{a+b}{n}\right) \cdot\left(\frac{a+c}{n}\right)+\left(\frac{d+b}{n}\right) \cdot\left(\frac{d+c}{n}\right)
+# $$
+
+# %%
+
+E = ((a + b)/ n) * ((a + c)/ n) * ((d + b)/ n) * ((a + b)/ n) * ((d + c)/ n) 
+
+print("“random luck” part of PC", E)
+
+# %% [markdown]
+# $$
+# H S S=\frac{P C-E}{1-E}
+# $$
+# %%
+
+HSS = (PC - E)/ (1 - E)
+print(" Heidke skill score", HSS)
+
+# %% [markdown]
+# $$
+# H=\frac{a}{a+c}
+# $$
+
+# %%
+
+H = a / (a + c)
+
+print("Hit rate",H)
+
+# %% [markdown]
+# $$
+# F=\frac{b}{b+d}
+# $$
+# %%
+F = b / (b + d)
+print ("false-alarm rate", F)
+
+# %% [markdown]
+# $$
+# F A R=\frac{b}{a+b}
+# $$
+
+# %%
+
+FAR = b / (a +b)
+print("false-alarm ratio", FAR)
+
+# %% [markdown]
+# $$
+# T S S=H-F
+# $$
+# %%
+TSS = H - F
+print("true skill score", TSS)
+
+# %% [markdown]
+# $$
+# C S I=\frac{a}{a+b+c}
+# $$
+
+# %%
+
+CSI = a / (a + b +c)
+print("critical success index", CSI)
+
+
+# %% [markdown]
+# $$
+# a_{r}=\frac{(a+b) \cdot(a+c)}{n}
+# $$
+
+# %%
+ar = ((a + b) * (a + c))/ n
+print("Hits that might have occurred by random chance", ar)
+
+# %% [markdown]
+# $$
+# G S S=\frac{a-a_{r}}{a-a_{r}+b+c}
+# $$
+
+# %%
+
+GSS = (a - ar)/ (a - ar + b + c)
+print("Gilbert’s skill score", GSS)
+
+# %% [markdown]
+# A21. Given forecasts having the contingency table
+# of exercise N20. Protective cost is 5k dollars to avoid a loss
+# of 50k dollars. Climatological frequency of the event is
+# 50%. 
+# 
+# - a) Find the value of the forecast. 
+
+
+
+
+# %% [markdown]
+# $$
+# E_{\text {climate}}=\min (C, o \cdot L)
+# $$
+# %%
+C, L, o = 5, 50, 0.5
+
+if C < (L*o):
+        E_climate = C
+        print(f'E_climate {E_climate}k dollars')
+else:
+        E_climate = L*o
+        print(f"E_climate {E_climate}K dollars")
+
+# %% [markdown]
+# $$
+# E_{\text {forecast}}=\frac{a}{n} C+\frac{b}{n} C+\frac{c}{n} L
+# $$
+# %%
+
+E_forecast = (a/n)*C + (b/n)*C + (c/n)*L 
+print(f"E_forecast {round(E_forecast,3)}k dollars")
+
+# %% [markdown]
+# $$
+# E_{p e r f e c t}=o \cdot C
+# $$
+
+# %%
+E_perfect = o*C 
+print(f"E_perfect {round(E_perfect,3)}k dollars")
+
+# %% [markdown]
+# $$
+# V=\frac{E_{\text {climate}}-E_{\text {forecast}}}{E_{\text {climate}}-E_{\text {perfect}}}
+# $$
+
+# %%
+
+V = (E_climate - E_forecast)/ (E_climate - E_perfect)
+print(f"Economic value {round(V,3)}")
+
+# %% [markdown]
+#  - a) cont.
+# **The economic value is negative which, indicates your forecast is worst than the climatology.... Time to rethink your job.** 
+# %% [markdown]
+# $$
+# r_{C L}=C / L
+# $$
+# %%
+r_cl = C/L
+print(f"cost/loss ratio {r_cl}")
+
+# %% [markdown]
+# - (b) If you
+# can get probabilistic forecasts, then what probability
+# would you want in order to decide to take protective
+# action? <br />
+#  <br />
+# **You should take protective action whenever the forecast probability
+# p of the event exceeds your cost/loss ratio. In this case. Where 
+# p > 0.1**
+# %% [markdown]
+# A22. Given the table below of k = 1 to 20 forecasts
+# of probability pk that 24-h accumulated precipitation
+# will be above 25 mm, and the verification ok = 1 if the
+# observed precipitation was indeed above this threshold. 
+# $$
+# \begin{array}{llllll}k & p_{k} & o_{k} & k & p_{k} & o_{k} \\ 1 & 0.9 & 1 & 11 & 0.4 & 0 \\ 2 & 0.85 & 1 & 12 & 0.35 & 0 \\ 3 & 0.8 & 0 & 13 & 0.3 & 1 \\ 4 & 0.75 & 1 & 14 & 0.25 & 0 \\ 5 & 0.7 & 1 & 15 & 0.2 & 0 \\ 6 & 0.65 & 1 & 16 & 0.15 & 1 \\ 7 & 0.6 & 0 & 17 & 0.1 & 0 \\ 8 & 0.55 & 1 & 18 & 0.05 & 0 \\ 9 & 0.5 & 0 & 19 & 0.02 & 0 \\ 10 & 0.45 & 1 & 20 & 0 & 0\end{array}
+# $$
+# - (a) Find the Brier skill score. 
+# $$
+# B S S=1-\frac{\sum_{k=1}^{N}\left(p_{k}-o_{k}\right)^{2}}{\left(\sum_{k=1}^{N} o_{k}\right) \cdot\left(N-\sum_{k=1}^{N} o_{k}\right)}
+# $$
+# %%
+k = np.arange(1,21,1)
+pk = np.array([0.9, 0.85, 0.8, 0.75, 0.7, 0.65, 0.6, 0.55, 0.5, 0.45, 0.4, 0.35, 0.3, 0.25, 0.2, 0.15, 0.1, 0.05, 0.02, 0.0])
+ok = np.array([1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0])
+N = float(len(k))
+k1 = np.sum((pk-ok)**2)
+k2 = (np.sum(ok) * (N - np.sum(ok)))
+
+BSS = 1 - (k1/k2)
+
+print(f"Brier skill score {BSS}")
+# %% [markdown]
+# - (b) For probability bins of width ∆p = 0.2, plot a reliability diagram, and
+# $$
+# j = round(p_k/ \Delta p, 0)
+# $$
+# %%
+dp = 0.2
+
+j = np.rint(pk/dp)
+J = np.arange(np.min(j), np.max(j)+1,1)
+print(f"Number of bins {len(J)}")
+pj = np.linspace(0, 1, len(J), endpoint=True)
+
+oj = pk[ok==1]
+
+noj_0, oj_0 = pk[np.where(pk<=0.+(dp/2))], oj[np.where(oj<=0.+(dp/2))]
+noj_1, oj_1 = pk[(np.where((pk >= 0.25-(dp/2)) & (pk <= 0.25+(dp/2))))], oj[(np.where((oj >= 0.25-(dp/2)) & (oj <= 0.25+(dp/2))))]
+noj_2, oj_2 = pk[(np.where((pk >= 0.5-(dp/2)) & (pk <= 0.5+(dp/2))))], oj[(np.where((oj >= 0.5-(dp/2)) & (oj <= 0.5+(dp/2))))]
+noj_3, oj_3 = pk[(np.where((pk >= 0.75-(dp/2)) & (pk <= 0.75+(dp/2))))], oj[(np.where((oj >= 0.75-(dp/2)) & (oj <= 0.75+(dp/2))))]
+noj_4, oj_4 = pk[np.where(pk>=1.-(dp/2))], oj[np.where(oj>=1.-(dp/2))]
+
+nj = np.array([len(noj_0),len(noj_1), len(noj_2), len(noj_3), len(noj_4)])
+noj = np.array([len(oj_0),len(oj_1), len(oj_2), len(oj_3), len(oj_4)])
+
+
+# %%
+fig, ax = plt.subplots(1,1, figsize=(10,8))
+fig.suptitle('Reliability diagram', fontsize= plt_set.title_size, fontweight="bold")
+ax.set_ylabel(r'$ \frac{n_oj}{n_j}$', fontsize = plt_set.label)
+ax.set_xlabel('$p_j$', fontsize = plt_set.label)
+ax.xaxis.grid(color='gray', linestyle='dashed')
+ax.yaxis.grid(color='gray', linestyle='dashed')
+ax.plot(pj, (noj/nj))
+x = np.linspace(0,1,100)
+ax.plot(x, x)
+
+
+plt.show()
+
+
+# %% [markdown]
+#  - (c) find the reliability Brier skill score.
+# $$
+# B S S_{\text {reliability}}=\frac{\sum_{j=0}^{J}\left[\left(n_{j} \cdot p_{j}\right)-n_{o j}\right]^{2}}{\left(\sum_{k=1}^{N} o_{k}\right) \cdot\left(N-\sum_{k=1}^{N} o_{k}\right)}
+# $$
+# %%
+k1 = np.sum((nj * pj) - noj)**2
+k2 = (np.sum(ok)) * (N - np.sum(ok))
+
+BSSR = k1/k2
+
+print(f"Reliability Brier skill score {round(BSSR,4)}")
+
+# %%
+# %% [markdown]
+# A23. For any one part of this exercise (Ex a to Ex
+# d) of this problem, a 10-member ensemble forecast
+# system forecasts probabilities that 24-h accumulated
+# rainfall will exceed 5 mm. The observation flags (o)
+# and forecast probabilities (p) are given in the table
+# (in the next column) for a 30-day period. Calculate
+# the hit rate and false-alarm rate for the full range
+# of allowed probability thresholds, and plot the result as a ROC diagram. Also find the area under the
+# ROC curve and find the ROC skill score
+# $$
+# \begin{array}{c|c|c|c|c|cc|c|c|c|c|c}\text { Day } & o & \text{Ex a:  } p(\%) & p(\%) & p(\%) & p(\%) & \text { Day } & o & \text{Ex a:  } p(\%) & p(\%) & p(\%) & p(\%) \\ \hline 1 & 1 & 50 & 10 & 100 & 0 & 16 & 0 & 60 & 30 & 20 & 40 \\ 2 & 0 & 20 & 0 & 0 & 10 & 17 & 1 & 70 & 60 & 60 & 50 \\ 3 & 1 & 20 & 30 & 90 & 20 & 18 & 1 & 90 & 70 & 60 & 60 \\ \hline 4 & 1 & 60 & 40 & 90 & 30 & 19 & 1 & 80 & 80 & 60 & 70 \\ \hline 5 & 0 & 50 & 30 & 0 & 40 & 20 & 0 & 70 & 70 & 30 & 80 \\ \hline 6 & 0 & 20 & 40 & 0 & 50 & 21 & 0 & 10 & 80 & 30 & 90 \\ \hline 7 & 0 & 30 & 50 & 10 & 60 & 22 & 0 & 10 & 90 & 30 & 100 \\ \hline 8 & 1 & 90 & 80 & 80 & 70 & 23 & 0 & 0 & 0 & 40 & 10 \\ \hline 9 & 0 & 40 & 70 & 10 & 80 & 24 & 0 & 0 & 10 & 40 & 20 \\ \hline 10 & 1 & 30 & 100 & 80 & 90 & 25 & 1 & 80 & 40 & 50 & 30 \\ \hline 11 & 1 & 100 & 100 & 70 & 100 & 26 & 0 & 0 & 30 & 40 & 40 \\ \hline 12 & 0 & 10 & 0 & 10 & 0 & 27 & 0 & 0 & 40 & 0 & 50 \\ \hline 13 & 0 & 0 & 0 & 20 & 10 & 28 & 1 & 100 & 70 & 50 & 60 \\ \hline 14 & 0 & 10 & 10 & 20 & 20 & 29 & 0 & 10 & 60 & 0 & 70 \\ \hline 15 & 1 & 80 & 40 & 70 & 30 & 30 & 1 & 90 & 10 & 50 & 0\end{array}
+# $$
+# %%
+
+
+# %%
---
+++ py/hw11/test.py
@@ -0,0 +1,395 @@
+# %%
+# -*- coding: utf-8 -*-
+# Eve Wicksteed
+# HW 11
+# NWP - ATSC 507
+
+
+# %% load libraries
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+
+current_dir = os.getcwd()
+my_out_dir = str(current_dir)+'/HW11/'
+
+
+# %% Set up data
+
+# Each field (i.e., each weather map) below covers an
+# area from North to South and West to East.
+
+anal = np.array([[5.2, 5.3, 5.4, 5.3],[5.3, 5.4, 5.5, 5.4],[5.4, 5.5, 5.6, 5.5],[5.5, 5.6, 5.7, 5.6],[5.6, 5.7, 5.8, 5.7]])
+
+fcst = np.array([[5.3, 5.4, 5.5, 5.4],[5.5, 5.4, 5.5, 5.6],[5.6, 5.6, 5.6, 5.6],[5.8, 5.7, 5.6, 5.7],[5.9, 5.8, 5.7, 5.8]])
+
+verif = np.array([[5.3, 5.3, 5.3, 5.4],[5.4, 5.3, 5.4, 5.5],[5.5, 5.4, 5.5, 5.5],[5.7, 5.5, 5.6, 5.6],[5.8, 5.7, 5.6, 5.6]])
+
+clim = np.array([[5.4, 5.4, 5.4, 5.4],[5.4, 5.4, 5.4, 5.4],[5.5, 5.5, 5.5, 5.5],[5.6, 5.6, 5.6, 5.6],[5.7, 5.7, 5.7, 5.7]])
+
+# %% A19
+
+
+
+# %% a. Mean forecast error
+
+ME_fcst = np.mean(fcst-verif)
+
+print("the mean forecast error is", round(ME_fcst, 3))
+
+# %% b. mean persistence error
+
+ME_persist = np.mean(anal-verif)
+
+print("the mean persistence error is", round(ME_persist, 3))
+
+# %% c. mean absolute forecast error
+
+MAE_fcst = np.mean(np.abs(fcst-verif))
+
+print("the mean absolute forecast error is", round(MAE_fcst, 3))
+
+# %% d. mean squared forecast error
+
+MSE_fcst = np.mean((fcst-verif)**2)
+
+print("the mean squared forecast error is", round(MSE_fcst, 3))
+
+# %% e. mean squared climatology error
+
+MSE_clim = np.mean((clim-verif)**2)
+
+print("the mean squared forecast error is", round(MSE_clim, 3))
+
+
+# %% f. mean squared forecast error skill score
+
+MSE_ref = MSE_clim
+MSESS_fcst = 1 - (MSE_fcst/MSE_ref)
+
+print("the mean squared forecast error skill score is", round(MSESS_fcst, 3))
+
+
+# %% g. RMS forecast error
+
+RMSE_fcst = np.sqrt(MSE_fcst)
+
+print("the root mean squared forecast error is", round(RMSE_fcst, 3))
+
+
+# %% h. correlation coefficient between forecast and verification
+
+fprime = fcst - np.mean(fcst)
+vprime = verif - np.mean(verif)
+
+PCC_fv = (np.mean(fprime*vprime)) / ( (np.sqrt(np.mean(fprime**2))) * np.sqrt(np.mean(vprime**2)) )
+
+print("the Pearson correlation coefficient between forecast and verification is", round(PCC_fv, 3))
+
+# %% i. forecast anomaly correlation
+
+AC = np.mean( ((fcst - clim)- np.mean(fcst - clim)) * ((verif - clim)- np.mean(verif - clim)) )  / np.sqrt( np.mean(((fcst - clim) - np.mean(fcst - clim))**2) * np.mean(((verif - clim) - np.mean(verif - clim))**2) )
+
+print("the forecast anomaly correlation is", round(AC,3))
+
+# %% j. persistence anomaly correlation
+
+AC_persist = np.mean( ((anal - clim)- np.mean(anal - clim)) * ((verif - clim)- np.mean(verif - clim)) )  / np.sqrt( np.mean(((anal - clim) - np.mean(anal - clim))**2) * np.mean(((verif - clim) - np.mean(verif - clim))**2) )
+
+print("the persistance anomaly correlation is", round(AC_persist,3))
+
+
+# %% k. Draw height contours by hand for each field, to show locations of ridges and troughs.
+
+plt.contour(anal, linewidths=4)
+#plt.show()
+# plt.savefig(my_out_dir+'anal'+'.png')
+
+plt.contour(fcst, linewidths=4)
+#plt.show()
+# plt.savefig(my_out_dir+'fcst'+'.png')
+
+plt.contour(verif, linewidths=4)
+#plt.show()
+# plt.savefig(my_out_dir+'verif'+'.png')
+
+plt.contour(clim, linewidths=4)
+#plt.show()
+# plt.savefig(my_out_dir+'clim'+'.png')
+
+#np.savetxt(my_fig_dir"foo.csv", a, delimiter=",")
+
+
+
+# %% save values to pandas df
+
+# df = pd.DataFrame()
+
+# df['ME_fcst']= ME_fcst
+
+# data = {"ME_fcst": [ME_fcst],
+# 'ME_persist': [ME_persist],
+# 'MAE_fcst': [MAE_fcst],
+# 'MSE_fcst': [MSE_fcst],
+# 'MSE_clim': [MSE_clim],
+# 'MSESS_fcst': [MSESS_fcst],
+# 'RMSE_fcst': [RMSE_fcst],
+# 'PCC_fv': [PCC_fv],
+# 'AC': [AC],
+# 'AC_persist': [AC_persist]
+# }
+
+# df = pd.DataFrame(data) 
+
+# df.to_csv(my_out_dir+"error_metrics.csv")
+
+
+
+# %% A 20
+
+# have the following contingincy table:
+
+a = 150
+b = 65
+c = 50
+d = 100
+
+n = a + b + c + d
+
+E = ((a+b)/n)* ((a+c)/n) + ((d+b)/n)*((d+c)/n)
+
+a_random = ((a+b)*(a+c)) / (n)
+
+# %%
+# So now calculate all binary verification stats
+
+bias = (a+b) / (a+c)
+
+portion_correct = (a + d) / n
+
+HSS = (portion_correct - E) / (1-E)
+
+hit_rate = a / (a + c)
+
+FA_rate =  b/ (b+d)# false alarm rate
+
+FA_ratio = b / (a+b) # false alarm ratio
+
+TSS = hit_rate - FA_rate # true skill score
+
+CSI = a / (a+b+c) # critical success index
+
+GSS = (a - a_random) / (a - a_random + b + c) # gilber skill score
+
+# %% add to df
+
+# a20df = pd.DataFrame()
+
+
+# a20data = {"a": [a],
+# 'b': [b],
+# 'c': [c],
+# 'd': [d],
+# 'n': [n],
+# 'E': [E],
+# 'a_random': [a_random],
+# 'bias': [bias],
+# 'portion_correct': [portion_correct],
+# 'HSS': [HSS],
+# 'hit_rate': [hit_rate],
+# 'FA_rate': [FA_rate],
+# 'FA_ratio': [FA_ratio],
+# 'TSS': [TSS],
+# 'CSI': [CSI],
+# 'GSS': [GSS]
+# }
+
+# a20df = pd.DataFrame(a20data) 
+
+# a20df.to_csv(my_out_dir+"binary_verif_stats.csv")
+
+
+# %% A 21
+
+# a, b, c, and d are the same
+
+o = 0.5 # %
+
+cost = 5000  # protective cost
+loss = 50000
+
+
+e_clim = min(cost, o*loss)
+e_forcast = (a/n)*cost + (b/n)*cost + (c/n)*loss
+e_perfect = o*cost
+
+Value = (e_clim - e_forcast) / (e_clim - e_perfect)
+
+# or alternatively calculate like this:
+
+r_cl = cost / loss
+v = (min(r_cl, o) - (FA_rate * r_cl*(1-o)) + (hit_rate*(1-r_cl)*o) - o ) / (min(r_cl, o) - o*r_cl)
+
+
+# a21df = pd.DataFrame()
+
+# a21data = {"cost": [cost],
+# 'loss': [loss],
+# 'clim_freq': [o],
+# 'e_clim': [e_clim],
+# 'e_forcast': [e_forcast],
+# 'e_perfect': [e_perfect],
+# 'Value': [Value], 
+# 'r_cl': [r_cl], 
+# 'v_eq_20_55': [v]}
+
+# a21df = pd.DataFrame(a21data) 
+# a21df.to_csv(my_out_dir+"a21_v2.csv")
+
+
+# %% a 22
+
+k = np.arange(0,20,1)
+pk = np.append(np.arange(0.9, 0, -0.05), np.array([0.02, 0])) 
+ok = np.array([1,1,0,1,1,1,0,1,0,1,0,0,1,0,0,1,0,0,0,0])
+
+N = 20
+
+# %% A
+
+# Brier skill score:
+
+BSS = 1 - ( (sum((pk-ok)**2)) /  ( (sum(ok)) * (N - sum(ok))  ) ) 
+
+# %% B Reliabilty diagram
+
+# get bins:
+dp = 0.2
+bins = np.arange(0, 1.2, dp)
+
+# find which obs goes in which bin... 
+j = np.ceil((pk / dp))
+
+# and the number of obs in each bin (nj)
+unique_j, nj = np.unique(j, return_counts=True)
+
+# get the observed probabilities that happened:
+oj = j[ok==1]
+unique_oj, n_oj = np.unique(oj, return_counts=True)
+
+# in this case we need to include in the array that 0 observations fall in bin 0
+final_noj = np.append(0, n_oj)
+
+# n_oj / nj
+portion_verified = final_noj / nj
+
+
+# %% create dataframes
+
+
+big_tbl_data = {"k": k,
+'pk': pk,
+'ok': ok,
+'bin': j}
+
+big_tbl = pd.DataFrame(big_tbl_data) 
+big_tbl.to_csv(my_out_dir+"a22_big_tbl.csv")
+
+
+sml_tbl_data = {"j": unique_j,
+'pj': bins,
+'nj': nj,
+'noj': final_noj,
+'noj/nj': portion_verified}
+
+sml_tbl = pd.DataFrame(sml_tbl_data) 
+sml_tbl.to_csv(my_out_dir+"a22_sml_tbl.csv")
+
+# %% plot
+
+# 1-1 line
+x = np.linspace(0,1,100)
+
+plt.plot(x, x, 'r--')
+plt.plot(bins, portion_verified, ".-")
+plt.xlabel("pj")
+plt.ylabel("noj / nj")
+plt.title("Reliability diagram")
+#plt.show()
+plt.savefig(my_out_dir+'a22_reliability'+'.png')
+
+
+# %% C find reliability brier skill score
+
+# pj (bin centre) = bins
+# nj = nj
+# noj = final_noj
+
+BSS_r = sum(((nj*bins)-final_noj)**2)  / (sum(ok)) * (N - sum(ok))
+
+results_a22_data = {"BSS": [BSS],
+'BSS_r': [BSS_r]}
+
+results_a22 = pd.DataFrame(results_a22_data) 
+results_a22.to_csv(my_out_dir+"a22_results.csv")
+
+# %% A 23 ROC diagram
+
+# part c
+# a23_hit_rate = [1.00,1.00,1.00,1.00,1.00,1.00,0.77,0.54,0.38,0.23,0.08]
+# a23_false_alarm_rate = [0.71,0.71,0.53,0.35,0.18,0.00,0.00,0.00,0.00,0.00,0.00]
+
+# part a
+a23_hit_rate = [1.00,1.00,1.00,0.86,0.85,0.85,0.77,0.69,0.62,0.38,0.15]
+a23_false_alarm_rate = [1.00,0.71,0.41,0.25,0.24,0.18,0.12,0.06,0.00,0.00,0.00]
+
+
+plt.plot(x,x)
+plt.plot(a23_false_alarm_rate, a23_hit_rate, ".-")
+plt.xlabel("False alarm rate")
+plt.ylabel("Hit rate")
+plt.title("ROC diagram")
+#plt.show()
+plt.savefig(my_out_dir+'a23_ROC_a'+'.png')
+
+
+# %% area under curve:
+
+import numpy as np
+from scipy.integrate import simps
+from numpy import trapz
+
+
+# The y values.  A numpy array is used here,
+# but a python list could also be used.
+y = a23_hit_rate
+
+# Compute the area using the composite trapezoidal rule.
+area = trapz(np.flip(a23_hit_rate), x = np.flip(a23_false_alarm_rate))
+print("area =", area)
+
+# Compute the area using the composite Simpson's rule.
+area = simps(a23_hit_rate, x=np.flip(a23_false_alarm_rate))
+print("area =", area)
+
+
+# from sklearn import metrics
+# y = np.array([1, 1, 2, 2])
+# pred = np.array([0.1, 0.4, 0.35, 0.8])
+# fpr, tpr, thresholds = metrics.roc_curve(a23_hit_rate, a23_false_alarm_rate, pos_label=2)
+# metrics.auc(fpr, tpr)
+
+# obs_y = [1,0,1,1,0,0,0,1,0,1,1,0,0,0,1,0,1,1,1,0,0,0,0,0,1,0,0,1,0,1]
+
+# probs = [50,20,20,60,50,20,30,90,40,30,100,10,0,10,80,60,70,90,80,70,10,10,0,0,80,0,0,100,10,90]
+# calculate roc curve
+# fpr, tpr, thresholds = metrics.roc_curve(obs_y, probs) # false alarms, # hit rate
+
+# plt.plot(fpr, tpr)
+# plt.show()
+
+# calculate AUC
+# auc = metrics.roc_auc_score(obs_y, probs)
+# print('AUC: %.3f' % auc)
